e3ce70aa48e478dfc366b80c26521fecf535cf46
Ezequiel Balasch <ezebalasch@gmail.com>
2022-09-20 22:00:23 -0300

    Prueba 2

---
+++ src/components/public/Footer.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const Footer = () => {
+  const [clicks, setClicks] = useState(0);
+  const year = new Date().getFullYear();
+  const companyName = "Ezequiel Balasch";
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <span onClick={handleClick}>
+          <img className="App-logo" height="52" src="mascota.png"></img>
+        </span>
+        <p className="col-md-4 mb-0 text-muted">
+          &copy; {year} {companyName} Clicks={clicks}
+        </p>
+
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
